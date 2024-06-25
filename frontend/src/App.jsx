fa369cd7dfa7bc2f568a0d22991ce5571241e6ce
Nyochembeng Enzo Nkengafack <nyochembengn@gmail.com>
2024-06-25 14:53:03 +0200

    vercel deploy frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,44 @@
-import './App.css';
-import Header from './components/header/Header';
-import Navbar from './components/navbar/Navbar';
-import About from './components/about/About';
-import Experience from './components/experience/Experience';
-import Services from './components/services/Services';
-import Portfolio from './components/portfolio/Portfolio';
-import Testimonial from './components/testimonial/Testimonial';
-import Contact from './components/contact/Contact';
-import Footer from './components/footer/Footer';
+import "./App.css";
+import Header from "./components/header/Header";
+import Navbar from "./components/navbar/Navbar";
+import About from "./components/about/About";
+import Experience from "./components/experience/Experience";
+import Services from "./components/services/Services";
+import Portfolio from "./components/portfolio/Portfolio";
+import Testimonial from "./components/testimonial/Testimonial";
+import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 const App = () => {
   return (
     <>
-      <header id="header"><Header/></header>
-      <Navbar/>
-      <section id="about"><About/></section>
-      <section id="experience"><Experience/></section>
-      <section id="services"><Services/></section>
-      <section id="portfolio"><Portfolio/></section>
-      <section id="testimonial"><Testimonial/></section>
-      <section id="contact"><Contact/></section>
-      <Footer/>
+      <header id="header">
+        <Header />
+      </header>
+      <Navbar />
+      <section id="about">
+        <About />
+      </section>
+      <section id="experience">
+        <Experience />
+      </section>
+      <section id="services">
+        <Services />
+      </section>
+      <section id="portfolio">
+        <Portfolio />
+      </section>
+      <section id="testimonial">
+        <Testimonial />
+      </section>
+      <section id="contact">
+        <Contact />
+      </section>
+      <Footer />
+      <SpeedInsights />
     </>
   );
-}
+};
 
 export default App;
