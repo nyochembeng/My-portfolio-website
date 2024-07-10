834e9d8c162ca85ba0f42d7833a00b661ad35108
Nyochembeng Enzo Nkengafack <nyochembengn@gmail.com>
2024-07-10 12:15:36 +0100

    about

--- frontend/src/components/about/About.jsx
+++ frontend/src/components/about/About.jsx
@@ -1,21 +1,21 @@
-import React from 'react'
-import './about.css'
-import CTA from '../header/CTA'
-import moi from '../../assets/Me.jpg'
-import { FaAward } from 'react-icons/fa'
-import { FiUsers } from 'react-icons/fi'
-import { VscFolderLibrary } from 'react-icons/vsc'
+import React from "react";
+import "./about.css";
+import CTA from "../header/CTA";
+import moi from "../../assets/Me.jpg";
+import { FaAward } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { VscFolderLibrary } from "react-icons/vsc";
 
 const About = () => {
   return (
-    <section id="about" className='about_section'>
+    <section id="about" className="about_section">
       <h5>Get to know</h5>
       <h2>About me</h2>
       <div className="container about__container">
         <div className="about__me">
           <div className="about__me__image">
             <img src={moi} alt="Me " />
-            <span className='about_img_hover'></span>
+            <span className="about_img_hover"></span>
           </div>
         </div>
         <div className="about__me__content">
@@ -37,13 +37,18 @@ const About = () => {
             </article>
           </div>
           <p>
-            I am a Software Engineering student at the University of Buea, in the Faculty of Engineering and Technology. Also, a Software Developer specializing in the building of efficient, scalable and robust web and mobile applications. Acquired various skills in various domains over the past years which are; Web Development (HMTL, CSS, JavaScript), Programming (Python, Java, C/C++), Database Administration (SQL) and Software Maintenance.
+            I am a Software Engineering student at the University of Buea, in
+            the Faculty of Engineering and Technology. Also, a Software
+            Developer specializing in the building of efficient, scalable and
+            robust web and mobile applications. Acquired various skills in
+            various domains over the past years which are: Problem-Solving, Team
+            Collaboration, Technical Documentation and Programming.
           </p>
-          <CTA/>
+          <CTA />
         </div>
       </div>
     </section>
   );
-}
+};
 
-export default About
+export default About;
